c533823e7e2029a29371fa1890a91ed2c9bdbff7
Long Vuong <longvuong@live.com>
2021-11-01 22:41:44 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// ILL GOTTEN GAIN FROM W3SCHOOL EXAMPLE
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+// document.getElementById("navbar").style.transition = "top 0.5s linear";
